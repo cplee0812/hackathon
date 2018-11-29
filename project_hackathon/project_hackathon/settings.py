fe5d908481db042a9fe769d7d448f80ea0386528
Steven Lin <stevenlin@StevendeMacBook-Air.local>
2018-11-29 23:54:56 +0800

    settings.py

--- project_hackathon/project_hackathon/settings.py
+++ project_hackathon/project_hackathon/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 django_heroku.settings(locals())
-=======
+#=======
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
->>>>>>> a118dc05ddc7817a60e5bcb215c0f851c7152070
+#>>>>>>> a118dc05ddc7817a60e5bcb215c0f851c7152070
