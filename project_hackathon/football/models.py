ce673d5f3d025d9d7f49091450ff7a8c29c1dfa4
Cheng Ping <b06404031@ntu.edu.tw>
2018-12-07 01:28:17 +0800

    dbtest

--- project_hackathon/football/models.py
+++ project_hackathon/football/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+'''
+    class game_imfo(models.Model):
+        game_num = models.IntegerField()
+        game_time = models.DateTimeField()
+        game_place = models.CharField()
+
+        def __init__(self):
+            return self.game_num
+
+
+    class gamelog(models.Model):
+        
+        score = 
+
+
+    class livebroadcast(models.Model):
+        
+
+
+
+    # Create your models here.
+
+'''
