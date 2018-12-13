3e16e58d6877259b0dc2f1c1cadbde057d9849f0
Cheng Ping <b06404031@ntu.edu.tw>
2018-12-13 20:35:50 +0800

    updatemodel

--- project_hackathon/football/models.py
+++ project_hackathon/football/models.py
@@ -1,33 +1,6 @@
 from django.db import models
     
     
-#class match_imfo(models.Model):
-    #game_num = models.IntegerField()
-    #game_starttime = models.DateTimeField()
-
-        #兩個starttime，兩個endtime，
-    #game_place = models.CharField(max_length)
-
-    #def __init__(self):
-        #return self.game_num
-
-
-#class gamelog(models.Model):
-        
-    #score = 
-
-
-    #class livebroadcast(models.Model):
-        
-
-
-
-    # Create your models here.
-
-<<<<<<< HEAD
-=======
-# Create your models here.
-
 class Team(models.Model):
 	"""docstring for Player"""
 	name = models.CharField(max_length=20, blank=False)
@@ -51,4 +24,29 @@ class Match(models.Model):
 	team1 = models.ForeignKey(Team, on_delete=models.PROTECT, related_name='Home Team +')
 	team2 = models.ForeignKey(Team, on_delete=models.PROTECT, related_name='Away Team +')
 	score = models.IntegerField(blank=True)
->>>>>>> 71f748676a819045b5446a20c508ee4d10b5be63
+    
+#class match_imfo(models.Model):
+    #game_num = models.IntegerField()
+    #game_starttime = models.DateTimeField()
+
+        #兩個starttime，兩個endtime，
+    #game_place = models.CharField(max_length)
+
+    #def __init__(self):
+        #return self.game_num
+
+
+#class gamelog(models.Model):
+        
+    #score = 
+
+
+    #class livebroadcast(models.Model):
+        
+
+
+
+    # Create your models here.
+
+
+# Create your models here.    
