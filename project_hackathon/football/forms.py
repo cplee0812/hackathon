e6dd5cd8c169536a8f3907011afab8734fff0e9b
Frank <frankyen81y037@gmail.com>
2019-01-03 21:17:56 +0800

    yen0103

--- project_hackathon/football/forms.py
+++ project_hackathon/football/forms.py
@@ -6,4 +6,4 @@ from football.models import Team
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ('team_name','player_number','jersey_color', 'document', )
+        fields = ('team_name','player_number','jersey_color', 'jersey_image', )
